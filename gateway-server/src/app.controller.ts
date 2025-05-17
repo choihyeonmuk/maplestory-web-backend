b9fc718f998092133b8bac096963ef6a4978a80d
choihyeonmuk <chiyak535@gmail.com>
2025-05-17 14:25:11 +0900

    Feat: 메인 앱 포트 변수화 및 헬스 체크 API 추가

--- gateway-server/src/app.controller.ts
+++ gateway-server/src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOperation({
+    summary: 'Health check',
+    description: 'Check if the server is running',
+  })
+  @Get('health')
+  healthCheck(): string {
+    return 'OK';
   }
 }
